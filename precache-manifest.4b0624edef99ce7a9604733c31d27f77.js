03a9ea0b7831be8ff0d191f408cebcc63f77ed77
Jozsef88 <49954645+Jozsef88@users.noreply.github.com>
2020-04-02 08:56:08 +0100

    Updates

---
+++ precache-manifest.4b0624edef99ce7a9604733c31d27f77.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c531b44e47ccbde763f70b2912baca11",
+    "url": "/yellowsite/index.html"
+  },
+  {
+    "revision": "b77b56adc535ac293eb6",
+    "url": "/yellowsite/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "5255886f42fb84645593",
+    "url": "/yellowsite/static/css/main.c80927a7.chunk.css"
+  },
+  {
+    "revision": "b77b56adc535ac293eb6",
+    "url": "/yellowsite/static/js/2.2ced2081.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yellowsite/static/js/2.2ced2081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5255886f42fb84645593",
+    "url": "/yellowsite/static/js/main.e8fbea01.chunk.js"
+  },
+  {
+    "revision": "cec806aad8d087d6e5da",
+    "url": "/yellowsite/static/js/runtime-main.524d0f47.js"
+  },
+  {
+    "revision": "a31a51dc79fe5e2776b048eaa61d49e2",
+    "url": "/yellowsite/static/media/header-bg.a31a51dc.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/yellowsite/static/media/map-image.f40ed5b1.png"
+  }
+]);
